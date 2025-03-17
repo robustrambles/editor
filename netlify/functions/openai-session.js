e0e9c6bde70893dea7a6ec8777c1e96c67fa8514
Andrew Bridge <andrew@andrewhbridge.co.uk>
2025-03-17 19:50:08 +0000

    feat: add authentication checks with github before the two new functions can proceed

--- netlify/functions/openai-session.js
+++ netlify/functions/openai-session.js
@@ -1,5 +1,15 @@
 exports.handler = async function(request) {
     const DEV = process.env.NETLIFY_DEV === 'true';
+    try {
+        const cookies = parseCookie(request.headers.cookie);
+        const token = cookies[DEV ? 'token' : '__Host-github-token'];
+        const client = new Octokit({ auth: token });
+    } catch (error) {
+        return {
+            statusCode: 401,
+            body: DEV ? error.toString() : '',
+        };
+    }
     const r = await fetch("https://api.openai.com/v1/realtime/sessions", {
         method: "POST",
         headers: {
--- netlify/functions/parse-legacy-doc.js
+++ netlify/functions/parse-legacy-doc.js
@@ -2,6 +2,16 @@ const WordExtractor = require("word-extractor");
 
 exports.handler = async function(request) {
     const DEV = process.env.NETLIFY_DEV === 'true';
+    try {
+        const cookies = parseCookie(request.headers.cookie);
+        const token = cookies[DEV ? 'token' : '__Host-github-token'];
+        const client = new Octokit({ auth: token });
+    } catch (error) {
+        return {
+            statusCode: 401,
+            body: DEV ? error.toString() : '',
+        };
+    }
     // create a buffer from the request's body
     const buffer = Buffer.from(request.body, 'base64');
     // extract content
